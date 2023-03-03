6ffe6e7a02ff05b84c6d08322124a53ca8ff9014
Ruslan Gren <gren.ruslan@gmail.com>
2023-03-03 16:26:18 +0200

    update

--- src/Task1.java
+++ src/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
     public static void main(String[] arg){
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        if (n > 50 && n < 100) {
+        if (n >= 50 && n <= 100) {
             System.out.printf("Число %d міститься в проміжку (50; 100)", n);
         } else {
             System.out.printf("Число %d не міститься в проміжку (50; 100)", n);
